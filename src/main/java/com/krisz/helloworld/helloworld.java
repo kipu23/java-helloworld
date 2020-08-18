114eecfde3f955276a7db8e9a50e2ef6fb9ea12d
Krisztian Szep <krisztian.szep@dxc.com>
2020-08-18 16:52:20 +0200

    some comments added

--- src/main/java/com/krisz/helloworld/helloworld.java
+++ src/main/java/com/krisz/helloworld/helloworld.java
@@ -1,5 +1,21 @@
+package com.krisz.helloworld;
+
+//the class of the program
 public class Helloworld {
+	
+	//The string to output
+	private final String message = "Hello World!";
+	
+	//Constructor of the class
+	public Helloworld() {}
+	
+	//Main method, this will be executed first
     public static void main(String[] args) {
-        System.out.println("Hello World! This file has been moved.");
+        System.out.println(new Helloworld().getMessage());
     }
+
+	//Method to retrive the message string
+	private final String getMessage(){
+		return message;
+	}
 }
