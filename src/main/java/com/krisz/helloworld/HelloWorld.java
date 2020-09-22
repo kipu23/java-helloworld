f0a0223ee6b4829646928eb670e88e3a273f9ee1
Krisztian Szep <krisztian.szep@dxc.com>
2020-09-22 16:35:20 +0200

    refactor

--- src/main/java/com/krisz/helloworld/HelloWorld.java
+++ src/main/java/com/krisz/helloworld/HelloWorld.java
@@ -6,21 +6,22 @@ import java.util.Enumeration;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
-public class Helloworld {
+public class HelloWorld {
 
     public static void main(String[] args) throws IOException {
 
         System.out.println("Hello World!");
 
-		Enumeration<URL> resources = Helloworld.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
-		while (resources.hasMoreElements()) {
-			try {
+        try {
+			Enumeration<URL> resources = HelloWorld.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+			while (resources.hasMoreElements()) {
 				Manifest manifest = new Manifest(resources.nextElement().openStream());
 				Attributes attr = manifest.getMainAttributes();
-				System.out.println(attr.getValue("Git-SHA"));
-			} catch (IOException E) {
-				// handle
+				String shaNumber = attr.getValue("Git-SHA");
+				System.out.println("Git SHA number: " +  shaNumber);
 			}
+		} catch (IOException E) {
+        	System.err.println("Error: Manifest file not found!");
 		}
 
     }
